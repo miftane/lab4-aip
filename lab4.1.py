0c0cb11f794f3d35c8465e37d7553150e6a8a2b7
Merlin <146963793+miftane@users.noreply.github.com>
2026-04-05 17:17:24 +0300

    Create lab4.1.py

---
+++ lab4.1.py
@@ -0,0 +1,8 @@
+# 4.1.py
+password1 = input("Введите пароль: ")
+password2 = input("Подтвердите пароль: ")
+
+if password1 == password2:
+    print("Пароль принят")
+else:
+    print("Пароль не принят")
